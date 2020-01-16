ececa2bfbff02af7df9100fa25e165af20ae4246
sunhl <sunhl@qq.com>
2020-01-16 16:09:00 +0800

    jedis连接池相关

--- redisTrain/src/com/atguigu/redis/test/TestPool.java
+++ redisTrain/src/com/atguigu/redis/test/TestPool.java
@@ -1,4 +1,24 @@
 package com.atguigu.redis.test;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import util.JedisPoolUtil;
+
+// 28_尚硅谷__Redis_Jedis_JedisPool
 public class TestPool {
+    public static void main(String[] args) {
+        // 获取连接
+        JedisPool jedisPool = JedisPoolUtil.getJedisPoolInstance();
+        Jedis jedis = null;
+        try {
+            jedis = jedisPool.getResource();
+            jedis.set("aa", "bb");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 最后要释放连接
+            JedisPoolUtil.release(jedisPool, jedis);
+        }
+
+    }
 }
--- redisTrain/src/util/JedisPoolUtil.java
+++ redisTrain/src/util/JedisPoolUtil.java
@@ -1,4 +1,36 @@
 package util;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+// 28_尚硅谷__Redis_Jedis_JedisPool
+// 一个单例类,用来获取jedis连接池
 public class JedisPoolUtil {
+    private static volatile JedisPool jedisPool = null;
+
+    private JedisPoolUtil () {}
+
+    public static JedisPool getJedisPoolInstance () {
+        if(jedisPool == null) {
+            synchronized (JedisPoolUtil.class)
+            {
+                JedisPoolConfig poolConfig = new JedisPoolConfig();
+                poolConfig.setMaxActive(1000);
+                poolConfig.setMaxIdle(32);
+                poolConfig.setMaxWait(100*1000);
+                poolConfig.setTestOnBorrow(true);
+
+
+                jedisPool = new JedisPool(poolConfig,"192.168.13.128",6379);
+            }
+        }
+        return jedisPool;
+    }
+
+    // 释放占用的连接,放回连接池
+    public static void release(JedisPool jedisPool, Jedis jedis) {
+        if (jedis != null) {
+            jedisPool.returnResourceObject(jedis);
+        }
+    }
 }
